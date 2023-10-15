c7412a0842cedd0dd23951b79c598daf2d810aae
marwaghraizi <marwaghraizi@gmail.com>
2023-10-15 21:56:06 +0200

    data augmentation

---
+++ src/data_augmentation.py
@@ -0,0 +1,31 @@
+import os
+import cv2
+import sys
+import numpy as np
+from Augmentor import Pipeline
+
+# Define input and output directories
+input_dir = sys.argv[1]  # Replace with your input directory
+output_dir = sys.argv[2]  # Replace with your output directory
+
+# Ensure the output directory exists
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Create an augmentation pipeline
+p = Pipeline(input_dir, output_directory=output_dir)
+
+# Define augmentation operations (you can customize this)
+p.random_contrast(probability=0.5, min_factor=0.8, max_factor=1.2)
+p.random_brightness(probability=0.5, min_factor=0.8, max_factor=1.2)
+#p.random_flip_left_right(probability=0.5)
+p.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
+
+# Set the number of images you want to create (per original image)
+p.sample(10)  # Adjust the number as needed
+
+# Execute the augmentation operations
+p.process()
+
+print("Data augmentation complete.")
+
